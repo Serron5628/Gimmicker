9102435a409faec28aa3354566bb4d5cc0808070
mizuame60214 <m011903059@edu.teu.ac.jp>
2020-08-26 13:32:56 +0900

    EnemyMoveXとZをEnemyMoveに合体した。Inspectorから、X方向に動くかZ方向に動くかの指定ができる。

---
+++ Assets/Scripts/YouCanMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouCanMove : MonoBehaviour
+{
+    public GameObject[] enemys;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            for(int i = 0; i < enemys.Length; i++)
+            {
+                //enemys[i].gameObject.GetComponent<>
+            }
+        }
+    }
+}
