0864ee9167f2493d9d7508d74f161c89e7eb0bf7
mizuame60214 <m011903059@edu.teu.ac.jp>
2020-09-20 02:20:44 +0900

    ものすごくテキトーにゴール演出入れました。

---
+++ Assets/@Delete/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject player;
+    public Material glad;
+    GameObject shape;
+    GameObject skinMesh;
+    public CinemachineVirtualCamera goalCam;
+
+    private void Start()
+    {
+        shape = player.gameObject.transform.Find("Shape").gameObject;
+        skinMesh = shape.gameObject.transform.Find("hero").gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            skinMesh.GetComponent<SkinnedMeshRenderer>().material = glad;
+            GetComponent<Animator>().SetTrigger("Goal");
+            shape.gameObject.GetComponent<Animator>().SetTrigger("Glad");
+        }
+    }
+
+    void CameraChange()
+    {
+        goalCam.Priority = 100;
+    }
+}
