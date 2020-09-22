207f6ec9fa4201e5c594f1b11854761d12632795
mizuame60214 <m011903059@edu.teu.ac.jp>
2020-09-22 12:28:41 +0900

    持つブロックのスクリプト基礎完成。アニメーションで瞬間移動じゃなくしたり、アニメーション中や移動中は置けないようにしたりしなきゃけないけど、とりあえずお疲れのセーブ。 注意事項メモ＞ ・PaintにBoxColliderが二つあるのは斜めからは持てない用。Colliderのy数値を変える時は気を付けないと持った時にPlayerと接触しなくなっちゃう。 ・Wallはプレイヤーが突進してもはねかえす用。 ・CollideWall?はWallタグとSwitchタグに触れてると置けないようになってる。

---
+++ Assets/Scripts/Block/CollideWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideWall : MonoBehaviour
+{
+    HaveRockAndPutOn scr;
+
+    private void Start()
+    {
+        scr = transform.parent.gameObject.GetComponent<HaveRockAndPutOn>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Switch"))
+        {
+            scr.forwardWall = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Switch"))
+        {
+            scr.forwardWall = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Block/HaveRockAndPutOn.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HaveRockAndPutOn : MonoBehaviour
+{
+    public GameObject player;
+    bool isHadRock = false;
+    bool isTrigger = false;
+    float putOnX;
+    float putOnZ;
+    public float onHeadPos = 0.0f;
+    public float putOnYPos = 2.0f;
+    public bool forwardWall = false;
+    int spaceCount = 0;
+
+    void Update()
+    {
+        //どこを向いてるかで値を決める。移動の仕方によって変わる。
+        ImitationWhereLook();
+
+        Debug.Log(spaceCount);
+        if (isTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isHadRock && forwardWall) return;
+                spaceCount++;
+            }
+        }
+
+        HadRock();
+        //岩が持たれる・置かれる場所へ自分（岩）を移動させる。とキーを離した時の動作。
+        PutOnPosition();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //プレイヤーがColliderに入ってる時にスペースを押すと。
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isTrigger = false;
+        }
+    }
+
+    void HadRock()
+    {
+        if(spaceCount % 2 == 1)
+        {
+            gameObject.transform.rotation = player.transform.rotation;
+            isHadRock = true;
+        }
+    }
+
+    void PutOnPosition()
+    {
+        if (isHadRock)
+        {
+            gameObject.transform.position = new Vector3(player.transform.position.x, onHeadPos, player.transform.position.z);
+            //キーを押したら向いてる方向に置く。
+            if (spaceCount % 2 == 0 && forwardWall == false)
+            {
+                gameObject.transform.position = new Vector3(player.gameObject.transform.position.x + putOnX, putOnYPos, player.gameObject.transform.position.z + putOnZ);
+                isHadRock = false;
+                spaceCount = 0;
+            }
+        }
+    }
+
+    void ImitationWhereLook()
+    {
+        if (player.transform.rotation == Quaternion.Euler(0, 0, 0))
+        {
+            putOnX = 0.0f;
+            putOnZ = 1.0f;
+        }
+        if (player.transform.rotation == Quaternion.Euler(0, 180, 0))
+        {
+            putOnX = 0.0f;
+            putOnZ = -1.0f;
+        }
+        if (player.transform.rotation == Quaternion.Euler(0, -90, 0))
+        {
+            putOnX = -1.0f;
+            putOnZ = 0.0f;
+        }
+        if (player.transform.rotation == Quaternion.Euler(0, 90, 0))
+        {
+            putOnX = 1.0f;
+            putOnZ = 0.0f;
+        }
+    }
+}
